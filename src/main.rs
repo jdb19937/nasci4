b5c7f3c77c810f91709f5a2755fea2de57def9b1
Dan Brumleve <jdb1729@gmail.com>
2022-12-26 13:16:08 -0600

    learning how to parse byte buffers and share collections across threads

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use std::{
     env,
-    fs, fs::File,
-    io::{self, prelude::*, BufRead, BufReader, ErrorKind, Lines},
+    fs,
+    io::{prelude::*, BufRead, BufReader},
     net::{TcpListener, TcpStream, UdpSocket},
-    thread, path, path::Path,
+    thread,
     time::Duration,
+    collections::BTreeMap,
+    rc::Rc,
+    sync::{Arc, Mutex, RwLock},
 };
 use threadpool::ThreadPool;
 
-fn handle(mut c : TcpStream) {
+mod store;
+
+fn handle(mut c : TcpStream, ind : Arc<RwLock<BTreeMap<u64,u64>>>) {
     let buf_reader = BufReader::new(&mut c);
 
     let http_request: Vec<_> = buf_reader
@@ -19,40 +24,55 @@ fn handle(mut c : TcpStream) {
 
     println!("Request: {:#?}", http_request);
 
-    let status_line = "HTTP/1.1 200 OK";
+    let status = "HTTP/1.1 200 OK";
     let contents = fs::read_to_string("hello.html").unwrap();
     let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     c.write_all(response.as_bytes()).unwrap();
 }
 
-fn userver(us : UdpSocket) {
+fn userver(us : UdpSocket, indx : Arc<RwLock<BTreeMap::<u64,u64>>>) {
     let mut buf = [0u8; 1024];
 
     loop {
+        for elem in buf.iter_mut() { *elem = 0; }
         let (result, src_addr) = us.recv_from(&mut buf).expect("recv failed");
-        println!("I received {} bytes!", result);
-        us.send_to(b"hello", src_addr).expect("send_to failed");
+        if result != 16 {
+            continue;
+        }
+        
+        let mut k = u64::from_be_bytes(buf[0..8].try_into().unwrap());
+        let mut v = u64::from_be_bytes(buf[8..16].try_into().unwrap());
+        println!("I received {}={} from {}!", k,v, src_addr);
+        // us.send_to(b"hello", src_addr).expect("send_to failed");
+
+        let mut ind = indx.write().expect("can't get index");
+        ind.insert(k, v);
+//        if let Some(vp) = ind.get(&k) {
+//            let mut wp = vp.lock().expect("can't get element");
+//            *wp = v;
+//        }
     }
 }
 
 fn heartbeat(us : UdpSocket, peers : Vec<String>) {
     let mut buf = [0u8; 1024];
 
-    for peer in &peers {
-      println!("peer={}", peer);
-    }
-
     loop {
         println!("beat");
         thread::sleep(Duration::from_secs(5));
+        for peer in &peers {
+            us.send_to(b"hello", peer);
+        }
     }
 }
 
 
 fn main() {
+    let mut ind = Arc::new(RwLock::new(BTreeMap::<u64,u64>::new()));
+
     let args: Vec<String> = env::args().collect();
     let hostport = &args[1];
 
@@ -72,16 +92,18 @@ fn main() {
       return;
     }
 
-    let us0 = UdpSocket::bind(hostport).expect("bind failed");
+    let us0 = UdpSocket::bind(hostport).expect(&format!("bind failed { }", hostport));
     let us1 = us0.try_clone().expect("clone failed");
-    thread::spawn(|| { userver(us0); });
+    let ind1 = Arc::clone(&ind);
+    thread::spawn(|| { userver(us0, ind1); });
     thread::spawn(|| { heartbeat(us1, peers); });
 
-    let s = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let s = TcpListener::bind(hostport).unwrap();
 
     let pool = ThreadPool::new(8);
     for c in s.incoming() {
         let c = c.unwrap();
-	pool.execute(|| { handle(c); });
+        let ind2 = Arc::clone(&ind);
+	pool.execute(|| { handle(c, ind2); });
     }
 }
---
+++ src/store.rs
@@ -0,0 +1,11 @@
+mod store {
+    pub struct Store {
+      pub x : u32,
+    }
+
+    pub fn insert(s : Store, key: String, val: String) {
+    }
+    pub fn lookup(s : Store, key: String, val: String) -> bool {
+        return true;
+    }
+}
